e2dac8507e7bf157729f4185c87dfece49fedf44
Anderson Santos <anderson.santos.medeiros@hotmail.com>
2019-02-24 13:31:57 -0300

    Created app's basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -15,17 +15,70 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Calculadora de IMC"),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.refresh),
-            onPressed: () {},
-          )
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Calculadora de IMC"),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {},
+            )
+          ],
+        ),
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    Icons.person_outline,
+                    color: Colors.green,
+                    size: 150.0,
+                  )
+                ],
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.green,
+                ),
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                    labelText: "Peso (kg)",
+                    labelStyle: TextStyle(color: Colors.green)),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.green,
+                ),
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                    labelText: "Altura (cm)",
+                    labelStyle: TextStyle(color: Colors.green)),
+              ),
+              Container(
+                  height: 50.0,
+                  margin: EdgeInsets.only(top: 10.0),
+                  child: RaisedButton(
+                    onPressed: () {},
+                    color: Colors.green,
+                    child: Text(
+                      "Calcular",
+                      style: TextStyle(color: Colors.white, fontSize: 25.0),
+                    ),
+                  ))
+            ],
+          ),
+        ));
   }
 }
