4c35ba34f4d036490dd56b092cd1c9d873bf0a24
Anderson Santos <anderson.santos.medeiros@hotmail.com>
2019-02-24 14:36:19 -0300

    Finalizado o layout do app

--- lib/main.dart
+++ lib/main.dart
@@ -15,60 +15,61 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Calculadora de IMC"),
-          centerTitle: true,
-          backgroundColor: Colors.green,
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.refresh),
-              onPressed: () {},
-            )
-          ],
-        ),
-        backgroundColor: Colors.white,
-        body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 15.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Icon(
-                    Icons.person_outline,
-                    color: Colors.green,
-                    size: 150.0,
-                  )
-                ],
-              ),
-              TextField(
-                keyboardType: TextInputType.number,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  fontWeight: FontWeight.w500,
+      appBar: AppBar(
+        title: Text("Calculadora de IMC"),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {},
+          )
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  Icons.person_outline,
                   color: Colors.green,
-                ),
-                textAlign: TextAlign.center,
-                decoration: InputDecoration(
-                    labelText: "Peso (kg)",
-                    labelStyle: TextStyle(color: Colors.green)),
+                  size: 150.0,
+                )
+              ],
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.w500,
+                color: Colors.green,
               ),
-              TextField(
-                keyboardType: TextInputType.number,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.green,
-                ),
-                textAlign: TextAlign.center,
-                decoration: InputDecoration(
-                    labelText: "Altura (cm)",
-                    labelStyle: TextStyle(color: Colors.green)),
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                  labelText: "Peso (kg)",
+                  labelStyle: TextStyle(color: Colors.green)),
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.w500,
+                color: Colors.green,
               ),
-              Container(
-                  height: 50.0,
-                  margin: EdgeInsets.only(top: 10.0),
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                  labelText: "Altura (cm)",
+                  labelStyle: TextStyle(color: Colors.green)),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 15.0),
+              child: Container(
+                  height: 60.0,
                   child: RaisedButton(
                     onPressed: () {},
                     color: Colors.green,
@@ -76,9 +77,16 @@ class _HomeState extends State<Home> {
                       "Calcular",
                       style: TextStyle(color: Colors.white, fontSize: 25.0),
                     ),
-                  ))
-            ],
-          ),
-        ));
+                  )),
+            ),
+            Text(
+              "Info",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 25.0),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
